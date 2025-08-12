f443433e217cb3f3bc7a64d34f50577853c8b38b
mujahidraj <mujahidraj65@gmail.com>
2025-08-13 00:27:42 +0600

    accordion

--- script.js
+++ script.js
@@ -1,32 +1,30 @@
 document.getElementById("noakhali-donate-button").addEventListener("click", function () {
-    mainBalanceDeducted("noakhali-donated-amount", "noakhali-balance");
+    mainBalanceDeducted("noakhali-donated-amount", "noakhali-balance", "areaName1");
     document.getElementById("my_modal_5").showModal();
-
 })
 
 document.getElementById("feni-donate-button").addEventListener("click", function () {
-    mainBalanceDeducted("feni-donated-amount", "feni-balance");
+    mainBalanceDeducted("feni-donated-amount", "feni-balance", "areaName2");
     document.getElementById("my_modal_5").showModal();
-
-
-
 })
 
 document.getElementById("student-donate-button").addEventListener("click", function () {
-    mainBalanceDeducted("student-donated-amount", "student-balance");
+    mainBalanceDeducted("student-donated-amount", "student-balance", "areaName3");
     document.getElementById("my_modal_5").showModal();
-
 })
 
 
-document.getElementById("history-button").addEventListener("click",function(){
+document.getElementById("history-button").addEventListener("click", function () {
     document.getElementById("donation-div").classList.add("hidden");
-        document.getElementById("history-div").classList.remove("hidden");
-
+    document.getElementById("history-div").classList.remove("hidden");
+    document.getElementById("history-button").classList.add("bg-[#B4F461]");
+    document.getElementById("donation-button").classList.remove("bg-[#B4F461]");
 })
 
-document.getElementById("donation-button").addEventListener("click",function(){
+document.getElementById("donation-button").addEventListener("click", function () {
     document.getElementById("donation-div").classList.remove("hidden");
-        document.getElementById("history-div").classList.add("hidden");
+    document.getElementById("history-div").classList.add("hidden");
+    document.getElementById("history-button").classList.remove("bg-[#B4F461]");
+    document.getElementById("donation-button").classList.add("bg-[#B4F461]");
 
 })
--- utilities.js
+++ utilities.js
@@ -1,15 +1,22 @@
-
-function mainBalanceDeducted (id,areaId){
+function mainBalanceDeducted(id, areaId, areaName) {
     const donatedBalance = document.getElementById(id).value;
     const profileBalance = document.getElementById("main-balance");
-      profileBalance.innerText = parseFloat(profileBalance.innerText) - parseFloat(donatedBalance);
+    profileBalance.innerText = parseFloat(profileBalance.innerText) - parseFloat(donatedBalance);
+    const donationAreaName = document.getElementById(areaName).innerText;
+    const noakhaliBalance = document.getElementById(areaId);
+    const now = new Date();
+    noakhaliBalance.innerText = parseFloat(noakhaliBalance.innerText) + parseFloat(donatedBalance);
+    const modalsPara = document.getElementById("inspiration").innerText = "You have donated " + donatedBalance + " BDT to the " +donationAreaName+ " at " + now + " .Thank you !"
     
-       const noakhaliBalance = document.getElementById(areaId);
-       noakhaliBalance.innerText = parseFloat(noakhaliBalance.innerText) + parseFloat(donatedBalance);
-        const modalsPara = document.getElementById("inspiration").innerText = "You have donated " + donatedBalance + " BDT to the Humankind .Thank you !"
-
-      return profileBalance.innerText && noakhaliBalance.innerText && modalsPara;
-        
-
+    const historyAdd = document.createElement("div");
+    historyAdd.innerHTML = `
+        <div class="p-8">
+                <h3 class="text-xl font-bold pb-4">${donatedBalance} Taka is Donated for ${donationAreaName}</h3>
+                <p class="text-[#111111B2] font-light">Date :${now} </p>
+            </div>
+        `
+    const history = document.getElementById("history-div").appendChild(historyAdd)
+    return profileBalance.innerText && noakhaliBalance.innerText && modalsPara && history;
 }
+
 
